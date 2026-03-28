c1a1b104cf7cb937268cd2797af011623a332531
Jonathan Gos <jonathan.gos@epfl.ch>
2026-03-28 19:18:02 +0100

    fix: avoid double conversions, improve resolution

--- firmware/arduino_mega2560/include/robot_config.h
+++ firmware/arduino_mega2560/include/robot_config.h
@@ -29,7 +29,7 @@
 // --- ADC properties ---
 #define ARDUINO_ADC_RESOLUTION_BITS     10
 #define ARDUINO_ADC_MAX_VALUE           ((1 << ARDUINO_ADC_RESOLUTION_BITS) - 1) // 1023 for 10-bit ADC
-#define ARDUINO_ADC_VOLTAGE_REF         5000 // in millivolts (5V reference voltage)
+#define ARDUINO_ADC_VOLTAGE_REF         5.0f // in millivolts (5V reference voltage)
 
 // --- PWM configuration ---
 #define ARDUINO_PWM_MOTOR_PRESCALER     0b010 // For ~4 kHz PWM frequency on Arduino Mega 2560 (Timer4)
@@ -48,15 +48,15 @@
 #define MOTOR_CW_DIRECTION              1
 
 // --- PWM parameters ---
-#define ESCON_PWM_DUTY_CYCLE_MIN       10.0f
-#define ESCON_PWM_DUTY_CYCLE_MAX       90.0f
+#define ESCON_PWM_DUTY_CYCLE_MIN       10.0f / 100.0f * 255 // Minimum duty cycle to overcome motor deadzone (10% of 255)
+#define ESCON_PWM_DUTY_CYCLE_MAX       90.0f / 100.0f * 255 // Maximum duty cycle to avoid overdriving the motor (90% of 255)
 #define ESCON_PWM_SPEED_RPM_MIN         0.0f
 #define ESCON_PWM_SPEED_RPM_MAX        MOTOR_MAX_PERMISSIBLE_RPM
 
 // --- Analog output configuration ---
 // Arduino ADC read 0-5V, so ESCON MUST output 0-4V only
-#define ESCON_ANALOG_VOLTAGE_MAX        4000 // in millivolts (4V max output from ESCON)
-#define ESCON_ANALOG_VOLTAGE_MIN        0 // in millivolts (0V min output from ESCON)
+#define ESCON_ANALOG_VOLTAGE_MAX        4.0f // in millivolts (4V max output from ESCON)
+#define ESCON_ANALOG_VOLTAGE_MIN        0.0f // in millivolts (0V min output from ESCON)
 
 #define ESCON_CURRENT_AT_VOLTAGE_MAX    3060 // in milliamps (3.06A from ESCON)
 #define ESCON_CURRENT_AT_VOLTAGE_MIN    -3060 // in milliamps (-3.06A from ESCON)

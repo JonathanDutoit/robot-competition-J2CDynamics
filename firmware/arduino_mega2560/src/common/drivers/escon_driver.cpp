21e0e7ccd7a6a76a88f8df3c301a0049d6f6f9db
Jonathan Gos <jonathan.gos@epfl.ch>
2026-03-30 14:39:29 +0200

    feat: send target command rather than RPM

--- firmware/arduino_mega2560/src/common/drivers/escon_driver.cpp
+++ firmware/arduino_mega2560/src/common/drivers/escon_driver.cpp
@@ -45,7 +45,7 @@ uint8_t EsconDriver::isReady() {
     return digitalRead(_readyPin) == LOW;
 }
 
-void EsconDriver::setSpeed(int16_t targetRpm) {
+void EsconDriver::setSpeed(int16_t targetCmd) {
     if (!isReady()) {
         digitalWrite(_enPin, LOW);
         analogWrite(_pwmPin, 0);
@@ -55,29 +55,18 @@ void EsconDriver::setSpeed(int16_t targetRpm) {
     }
 
     // Direction Logic
-    if (targetRpm >= 0) {
+    if (targetCmd >= 0) {
         digitalWrite(_dirPin, MOTOR_CCW_DIRECTION);
     } else {
         digitalWrite(_dirPin, MOTOR_CW_DIRECTION);
-        targetRpm = -targetRpm;
+        targetCmd = -targetCmd;
     }
 
     // Saturation
-    if (targetRpm > MOTOR_MAX_PERMISSIBLE_RPM) targetRpm = MOTOR_MAX_PERMISSIBLE_RPM;
+    int8_t cmd = constrain(targetCmd, ESCON_PWM_DUTY_CYCLE_MIN, 
+                            ESCON_PWM_DUTY_CYCLE_MAX);
 
-    // Convert RPM to Duty Cycle (0-255) and write to PWM pin
-    uint8_t duty = rpmToDuty(targetRpm);
-    analogWrite(_pwmPin, duty);
-}
-
-uint8_t EsconDriver::rpmToDuty(int16_t rpm) {
-    // Convert RPM to duty cycle (0-255) for analogWrite
-    return static_cast<uint8_t>(
-        map(rpm, 
-            0, MOTOR_MAX_PERMISSIBLE_RPM,
-            ESCON_PWM_DUTY_CYCLE_MIN, ESCON_PWM_DUTY_CYCLE_MAX
-        )
-    );
+    analogWrite(_pwmPin, cmd);
 }
 
 int16_t EsconDriver::getSpeed() {
--- firmware/arduino_mega2560/src/common/drivers/escon_driver.hpp
+++ firmware/arduino_mega2560/src/common/drivers/escon_driver.hpp
@@ -10,8 +10,8 @@ class EsconDriver : public ISpeedController {
                     uint8_t directionDigitalInputPin, uint8_t readyDigitalInputPin, 
                     uint8_t speedAnalogOutputPin, uint8_t currentAnalogOutputPin);
         void init();
-        void setSpeed(int16_t targetRpm);
-        int16_t getSpeed();
+        void setSpeed(int16_t targetCmd) override;
+        int16_t getSpeed() override;
         int16_t getCurrent();
     private:
         uint8_t _pwmPin, _enPin, _dirPin, _readyPin, _speedPin, _currPin;

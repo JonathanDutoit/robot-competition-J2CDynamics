7bc8197855c5c73439d9d3acacb854e7d87e98d3
Jonathan Dutoit <dutoit.jonathaneric@gmail.com>
2026-05-11 13:28:07 +0200

    simulation model correctly loaded to gazebo

--- ros2_ws/src/j2cdynamics_bringup/launch/turtlebot_simulation.launch.py
+++ ros2_ws/src/j2cdynamics_bringup/launch/turtlebot_simulation.launch.py
@@ -1,53 +1,117 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition, UnlessCondition
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    RegisterEventHandler, 
+    SetEnvironmentVariable
+)
+
+from launch.event_handlers import (
+    OnProcessExit,
+)
 from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+import ament_index_python.packages 
 
 
 def generate_launch_description():
 
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     pkg_share = FindPackageShare(package='turtlebot3_description').find('turtlebot3_description')
-    urdf = os.path.join(pkg_share, 'urdf', 'turtlebot3_burger.urdf')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'model.rviz')
+    urdf = os.path.join(pkg_share, 'urdf', 'turtlebot3_waffle.urdf')
+    rviz_config = os.path.join(pkg_share, 'rviz', 'model.rviz')
 
-    robot_desc = Command(['xacro ', LaunchConfiguration('model')])
+    gazebo_model_path = os.path.dirname(
+        ament_index_python.packages.get_package_share_directory('turtlebot3_description')
+    )
 
+    robot_description = {
+        'robot_description': Command(['xacro ', LaunchConfiguration('model')])
+    }
+
+    # Robot State Publisher
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[robot_description]
     )
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', urdf])}],
-        condition=UnlessCondition(LaunchConfiguration('gui'))
+
+    # Gazebo
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                FindPackageShare('gazebo_ros').find('gazebo_ros'),
+                'launch', 'gazebo.launch.py'
+            )
+        ]),
+        launch_arguments={
+            'verbose': 'true'   
+        }.items()
     )
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
+
+    # Spawn robot
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'turtlebot3', 
+                    '-x', '0.0',
+                    '-y', '0.0',
+                    '-z', '1',   
+                    '-R', '0.0',
+                    '-P', '0.0',
+                    '-Y', '0.0',],
+        output='screen'
     )
+
+    # Controllers
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster',
+            '--controller-manager',
+            '/controller_manager'
+        ]
+    )
+
+    diff_drive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'diff_drive_controller',
+            '--controller-manager',
+            '/controller_manager'
+        ], 
+        remappings=[
+        ('/diff_drive_controller/odom', '/odom'),
+        ('/diff_drive_controller/cmd_vel', '/cmd_vel'),
+    ]
+    )
+
+    # RViz
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            name='gui', 
-            default_value='True', 
-            description='Flag to enable joint_state_publisher_gui'),
+        SetEnvironmentVariable(
+            name='GAZEBO_MODEL_PATH',
+            value=gazebo_model_path + ':' + os.environ.get('GAZEBO_MODEL_PATH', '')
+        ),
+        SetEnvironmentVariable(
+            name='GAZEBO_MODEL_DATABASE_URI',
+            value=''
+        ),
 
         DeclareLaunchArgument(
             name='model', 
@@ -56,16 +120,31 @@ def generate_launch_description():
 
         DeclareLaunchArgument(
             name='rvizconfig', 
-            default_value=default_rviz_config_path, 
+            default_value=rviz_config, 
             description='Absolute path to rviz config file'),
 
         DeclareLaunchArgument(
             name='use_sim_time', 
             default_value=use_sim_time, 
             description='Use simulation (Gazebo) clock if true'),
+
         robot_state_publisher_node,
-        
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
-        rviz_node
+        gazebo, 
+        spawn_entity,
+
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
+        ), 
+
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[diff_drive_controller_spawner]
+            )
+        ),
+
+        #rviz_node
     ])

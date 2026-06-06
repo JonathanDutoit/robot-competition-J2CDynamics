8f1dc76f656ed2c59bfedd251ad03d3f9049d0af
JonathanDutoit <dutoit.jonathaneric@gmail.com>
2026-06-06 23:17:27 +0200

    camera, lidar and drivers working for second robot

--- ros2_ws/src/j2cdynamics_bringup/launch/camera.launch.py
+++ ros2_ws/src/j2cdynamics_bringup/launch/camera.launch.py
@@ -3,6 +3,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='j2cdynamics_camera', executable='camera_node'),
-        Node(package='j2cdynamics_camera', executable='duplo_approach'),
+        Node(package='j2cdynamics_camera', executable='camera_node', output='screen', emulate_tty=True),
+        Node(package='j2cdynamics_camera', executable='duplo_approach', output='screen', emulate_tty=True),
     ])

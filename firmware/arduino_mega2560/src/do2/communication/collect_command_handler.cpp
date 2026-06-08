b9241fdefebd2d1b2f77703e67102d68046e1e65
Jonathan Gos <jonathan.gos@epfl.ch>
2026-06-08 10:21:21 +0200

    correct sign error for sweepers

--- firmware/arduino_mega2560/src/do2/communication/collect_command_handler.cpp
+++ firmware/arduino_mega2560/src/do2/communication/collect_command_handler.cpp
@@ -6,7 +6,7 @@ CollectCommandHandler::CollectCommandHandler(Do2Command& cmd) : _cmd(cmd) {}
 bool CollectCommandHandler::handleCommand(const char* command, const float* args) {
     if (strcmp(command, "COLLECT") == 0) {
         _cmd.leftSweeperVelocity = DC_MOTOR_CRUISE_VEL_RAD_SEC; // Cruise speed for collection
-        _cmd.rightSweeperVelocity = DC_MOTOR_CRUISE_VEL_RAD_SEC; // Cruise speed for collection
+        _cmd.rightSweeperVelocity = -DC_MOTOR_CRUISE_VEL_RAD_SEC; // Cruise speed for collection
         return true;
     }
     return false;
